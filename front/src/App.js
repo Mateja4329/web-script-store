b83c5d677dee0de231d4017417ddbc1d06c1cf55
Mateja4329 <mateja.gl12@gmail.com>
2026-04-29 00:09:01 +0200

    Initial commit: Added React app

---
+++ front/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+          <h1>Dobro dosli u skriptarnicu FTN!</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
